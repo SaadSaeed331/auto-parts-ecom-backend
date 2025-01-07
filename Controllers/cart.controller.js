ec70c6158f564b30fc7d7a5fead9b8d39ed68dab
SaadSaeed331 <meetsaad331@gmail.com>
2025-01-07 15:40:20 +0500

    commit code

--- Controllers/cart.controller.js
+++ Controllers/cart.controller.js
@@ -1,5 +1,6 @@
 const orderModels = require("../models/order.models")
 const signSchema = require("../models/userdata.model")
+const stripe = require("stripe")("sk_test_51QeZeM4F3IBPvvICrgL2T3uIpQBNWRBAYDNoNWjqqcmWeOBdc0QCvVRkJfafFM0KAbbkG7LOOfTB3ktPmRMclk1L004uPTVUnG")
 
 const orderArray = async (req, res) => {
     console.log(req.body)
@@ -20,14 +21,60 @@ const orderArray = async (req, res) => {
         status
     }
     var resp = await orderModels.create(obj)
-    console.log(obj);
+    // console.log(obj);
 
 }
 
 const orderhistory = async (req, res) => {
     var resp = await orderModels.find({ costumerId: req.userData._id })
     res.send(resp)
-    console.log(resp);
+    // console.log(resp);
 
 }
-module.exports = { orderArray, orderhistory }
+
+const getadmindata = async (req, res) => {
+    var resp = await orderModels.find()
+    res.send(resp)
+    // console.log(resp);
+}
+
+const setstatus = async (req, res) => {
+    console.log(req.body);
+    
+    var resp = await orderModels.findByIdAndUpdate(req.body.id, {status: req.body.status})
+    res.send(resp)
+    console.log(resp);
+    
+}
+
+
+const makePayment = async (req, res) => {
+    const {products} = req.body;
+
+
+    const lineItems = products.map((product)=>({
+        price_data:{
+            currency:"usd",
+            product_data:{
+                name:product.name,
+                images:[product.prdtimg]
+            },
+            unit_amount:product.price * 100,
+        },
+        quantity:product.quantity
+    }));
+ 
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:["card"],
+        line_items:lineItems,
+        mode:"payment",
+        success_url:"http://localhost:5173/sucess",
+        cancel_url:"http://localhost:5173/cancel",
+    });
+
+    res.json({id:session.id})
+}
+
+
+
+module.exports = { orderArray, orderhistory,getadmindata ,setstatus, makePayment}
--- Routes/cart.routes.js
+++ Routes/cart.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { orderArray, orderhistory} = require("../Controllers/cart.controller")
-const {isLoggedin} = require("../middleware/isLoggedin")
+const { orderArray, orderhistory,getadmindata, setstatus, makePayment} = require("../Controllers/cart.controller")
+const {isLoggedin} = require("../middleware/isLoggedin");
+const { isAdmin } = require('../middleware/isAdmin');
 const cart = express.Router();
 
 cart.post("/orderArray",isLoggedin, orderArray)
 cart.get("/orderhistory",isLoggedin, orderhistory)
+cart.get("/getData",isLoggedin,isAdmin,getadmindata)
+cart.patch("/setstatus",isLoggedin,isAdmin,setstatus)
+
+cart.post("/payment",isLoggedin, makePayment)
+
 
 module.exports = cart;
